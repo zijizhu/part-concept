a80ee937283b59578567821ca273c461f4dd535d
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-03-24 18:15:00 +1100

    bug fix

--- model.py
+++ model.py
@@ -20,7 +20,7 @@ class PartCEM(nn.Module):
         conv_weights = self.concepts[..., None, None] # shape: [num_concepts + 1, c, 1, 1]
 
         x_norm = x / torch.linalg.vector_norm(x, ord=2, dim=1, keepdim=True)
-        conv_weights = torch.linalg.vector_norm(conv_weights, ord=2, dim=1, keepdim=True)
+        conv_weights = conv_weights / torch.linalg.vector_norm(conv_weights, ord=2, dim=1, keepdim=True)
         score_maps = F.sigmoid(F.conv2d(x_norm, conv_weights)) # shape: [b, num_concepts + 1, h, w]
         scores = F.sigmoid(score_maps[:, :-1, ...].sum((-1, -2))) # shape: [b, num_concepts]
 
