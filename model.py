3bab6736dc60571506d41476fe3c664c715a78c8
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-03-24 17:56:14 +1100

    update model calculation :(

--- model.py
+++ model.py
@@ -9,7 +9,8 @@ class PartCEM(nn.Module):
         self.backbone = timm.create_model(backbone, pretrained=True)
         self.dim = self.backbone.fc.weight.shape[-1]
         self.concepts = nn.Parameter(torch.randn(num_concepts + 1, self.dim))
-        self.fc = nn.Linear(num_concepts, num_classes)
+        # self.fc = nn.Linear(num_concepts, num_classes)
+        self.fc = nn.Linear(self.dim, num_classes)
     
     def forward(self, x):
         x = self.backbone.forward_features(x)
@@ -20,14 +21,16 @@ class PartCEM(nn.Module):
 
         score_maps = F.sigmoid(F.conv2d(x, conv_weights)) # shape: [b, num_concepts + 1, h, w]
         scores = F.sigmoid(score_maps[:, :-1, ...].sum((-1, -2))) # shape: [b, num_concepts]
-        preds = self.fc(scores) # shape: [b, num_classes]
 
         concepts_expanded = self.concepts[..., None, None].expand(-1, -1, h, w) # shape: [num_concepts + 1, c, h, w]
         reconstructed = torch.einsum('bkhw,kchw->bchw', score_maps, concepts_expanded) # shape: [b, c, h, w]
+
+        reconstructed_pooled = self.backbone.global_pool(reconstructed) # shape: [b, c]
+        preds = self.fc(reconstructed_pooled) # shape: [b, num_classes]
 
         x = x.view(b, c, h*w).permute(0, 2, 1) # shape: [b, h*w, c]
         reconstructed = reconstructed.view(b, c, h*w).permute(0, 2, 1) # shape: [b, h*w, c]
 
         recon_loss = torch.mean(torch.cdist(x.detach(), reconstructed, p=2)) # shape: [h*w, h*w] -> []
-        recon_commit_loss = torch.mean(torch.cdist(x, reconstructed.detach(), p=2))
-        return score_maps, scores, preds, (recon_loss + recon_commit_loss) * 0.1
+        recon_commit_loss = torch.mean(torch.cdist(x, reconstructed.detach(), p=2)) # shape: [h*w, h*w] -> []
+        return score_maps, scores, preds, recon_loss + recon_commit_loss
