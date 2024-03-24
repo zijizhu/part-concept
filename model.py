6546b3e02fc0530df283861a1d2e26c2db804bcd
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-03-24 21:01:00 +1100

    model update

--- model.py
+++ model.py
@@ -10,6 +10,8 @@ class PartCEM(nn.Module):
         self.dim = self.backbone.fc.weight.shape[-1]
         self.concepts = nn.Parameter(torch.randn(num_concepts + 1, self.dim))
         # self.fc = nn.Linear(num_concepts, num_classes)
+        self.concept_fc = nn.Linear(28*28, num_concepts)
+        self.sig = nn.Sigmoid()
         self.fc = nn.Linear(self.dim, num_classes)
     
     def forward(self, x):
@@ -23,7 +25,9 @@ class PartCEM(nn.Module):
         conv_weights = conv_weights / torch.linalg.vector_norm(conv_weights, ord=2, dim=1, keepdim=True)
         # score_maps = F.sigmoid(F.conv2d(x_norm, conv_weights)) # shape: [b, num_concepts + 1, h, w]
         score_maps = F.conv2d(x_norm, conv_weights) # shape: [b, num_concepts + 1, h, w]
-        scores = F.sigmoid(score_maps[:, :-1, ...].sum((-1, -2))) # shape: [b, num_concepts]
+        # scores = F.sigmoid(score_maps[:, :-1, ...].sum((-1, -2))) # shape: [b, num_concepts]
+        score_maps_flatten = score_maps[:, :-1, ...].view(b, -1, h*w)
+        scores = self.sig(self.concept_fc(score_maps_flatten)).squeeze(-1)
 
         concepts_expanded = self.concepts[..., None, None].expand(-1, -1, h, w) # shape: [num_concepts + 1, c, h, w]
         reconstructed = torch.einsum('bkhw,kchw->bchw', score_maps, concepts_expanded) # shape: [b, c, h, w]
