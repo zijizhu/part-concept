8ff0cb754140af678b3ea24ec01ce891b7cfc9bd
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-04-01 21:46:01 +1100

    debug

--- engine.py
+++ engine.py
@@ -32,21 +32,23 @@ def train_epoch(model, dataloader: DataLoader, optimizer: torch.optim,
         parts, maps, preds = model(imgs)
         preds = preds[:, 0:-1, :].mean(1)
         # Calculate all losses
-        cx, cy, grid_x, grid_y = landmark_coordinates(maps=maps, device=device)
-        loss_dict = dict(
-            label=F.cross_entropy(preds, labels, reduction='mean'),
+        # cx, cy, grid_x, grid_y = landmark_coordinates(maps=maps, device=device)
+        # loss_dict = dict(
+        #     label=F.cross_entropy(preds, labels, reduction='mean'),
             # conc=conc_loss(cx, cy, grid_x, grid_y, maps=maps),
             # orth=orth_loss(parts=parts.permute(0,2,1), device=device),
             # pres=1 - F.avg_pool2d(maps[:, :, 2:-2, 2:-2], 3, stride=1).max(-1)[0].max(-1)[0].max(0)[0].mean()
-        )
+        # )
         # Calculate total Loss
-        total_loss = sum(loss_coefs[k] * v for k, v in loss_dict.items())
+        # total_loss = sum(loss_coefs[k] * v for k, v in loss_dict.items())
+        total_loss = F.cross_entropy(preds, labels, reduction='mean')
 
         total_loss.backward()
         optimizer.step()
         optimizer.zero_grad()
         
          # Compute running losses and number of correct predictions
+        loss_dict = {'label': total_loss.detach().cpu()}
         for k, v in loss_dict.items():
             running_loss_dict[k] += loss_coefs[k] * v.item() * batch_size
 
