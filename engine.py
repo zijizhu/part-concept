7748bf490c0daa0c67f56be6a6cbb87e4b21201a
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-04-01 21:05:37 +1100

    debug training

--- engine.py
+++ engine.py
@@ -35,9 +35,9 @@ def train_epoch(model, dataloader: DataLoader, optimizer: torch.optim,
         cx, cy, grid_x, grid_y = landmark_coordinates(maps=maps, device=device)
         loss_dict = dict(
             label=F.cross_entropy(preds, labels, reduction='mean'),
-            conc=conc_loss(cx, cy, grid_x, grid_y, maps=maps),
-            orth=orth_loss(parts=parts.permute(0,2,1), device=device),
-            pres=1 - F.avg_pool2d(maps[:, :, 2:-2, 2:-2], 3, stride=1).max(-1)[0].max(-1)[0].max(0)[0].mean()
+            # conc=conc_loss(cx, cy, grid_x, grid_y, maps=maps),
+            # orth=orth_loss(parts=parts.permute(0,2,1), device=device),
+            # pres=1 - F.avg_pool2d(maps[:, :, 2:-2, 2:-2], 3, stride=1).max(-1)[0].max(-1)[0].max(0)[0].mean()
         )
         # Calculate total Loss
         total_loss = sum(loss_coefs[k] * v for k, v in loss_dict.items())
