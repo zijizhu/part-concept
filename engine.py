eca39c0e0c18bf2d35ff51de75acbd9086d77f5e
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-04-01 23:15:31 +1100

    add concept layers

--- engine.py
+++ engine.py
@@ -12,6 +12,7 @@ log = logging.getLogger(__name__)
 
 loss_coefs = dict(
     label=2.0,
+    concept=2.0,
     conc=1000.0,
     orth=1.0,
     pres=1.0
@@ -29,26 +30,26 @@ def train_epoch(model, dataloader: DataLoader, optimizer: torch.optim,
         imgs, labels, attrs = imgs.to(device), labels.to(device), attrs.to(device)
         batch_size = img_ids.size(0)
 
-        parts, maps, preds = model(imgs)
+        parts, maps, preds, cpt_preds = model(imgs)
+        preds = preds[:, 0:-1, :].mean(1)
         preds = preds[:, 0:-1, :].mean(1)
         # Calculate all losses
-        # cx, cy, grid_x, grid_y = landmark_coordinates(maps=maps, device=device)
-        # loss_dict = dict(
-        #     label=F.cross_entropy(preds, labels, reduction='mean'),
-            # conc=conc_loss(cx, cy, grid_x, grid_y, maps=maps),
-            # orth=orth_loss(parts=parts.permute(0,2,1), device=device),
-            # pres=1 - F.avg_pool2d(maps[:, :, 2:-2, 2:-2], 3, stride=1).max(-1)[0].max(-1)[0].max(0)[0].mean()
-        # )
+        cx, cy, grid_x, grid_y = landmark_coordinates(maps=maps, device=device)
+        loss_dict = dict(
+            label=F.cross_entropy(preds, labels, reduction='mean'),
+            label=F.binary_cross_entropy_with_logits(cpt_preds, attrs),
+            conc=conc_loss(cx, cy, grid_x, grid_y, maps=maps),
+            orth=orth_loss(parts=parts.permute(0,2,1), device=device),
+            pres=1 - F.avg_pool2d(maps[:, :, 2:-2, 2:-2], 3, stride=1).max(-1)[0].max(-1)[0].max(0)[0].mean()
+        )
         # Calculate total Loss
-        # total_loss = sum(loss_coefs[k] * v for k, v in loss_dict.items())
-        total_loss = F.cross_entropy(preds, labels, reduction='mean')
+        total_loss = sum(loss_coefs[k] * v for k, v in loss_dict.items())
 
         total_loss.backward()
         optimizer.step()
         optimizer.zero_grad()
         
          # Compute running losses and number of correct predictions
-        loss_dict = {'label': total_loss.detach().cpu()}
         for k, v in loss_dict.items():
             running_loss_dict[k] += loss_coefs[k] * v.item() * batch_size
 
