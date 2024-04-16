6c247b9407e10c174f43671a1e38a46b0694d699
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-04-16 10:11:12 +1000

    update setup

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ set -x
 
 mkdir -p datasets/CUB && cd datasets/CUB
 wget -q --show-progress "https://data.caltech.edu/records/65de6-vp158/files/CUB_200_2011.tgz"
-tar -zxf CUB_200_2011.tgz -C CUB --no-same-owner
+tar -zxf CUB_200_2011.tgz --no-same-owner
